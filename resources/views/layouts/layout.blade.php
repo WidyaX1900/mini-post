c814d20fde32c42ee1ce38e0c115bfb3d2704d54
WidyaX1900 <tuderangga93@gmail,com>
2025-08-02 11:59:38 +0800

    setup: connect vite to views

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -9,6 +9,7 @@
     <title>Mini Post App</title>
     <link rel="stylesheet" href="{{ config('app.asset_url') }}bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    @vite(['resources/js/app.js'])
 </head>
 
 <body>
