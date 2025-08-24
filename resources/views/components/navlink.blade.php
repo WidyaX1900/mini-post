7333565531765eaf03191b4370b8496673873c59
WidyaX1900 <tuderangga93@gmail,com>
2025-08-24 11:36:38 +0800

    delete: navlink components

--- resources/views/components/navlink.blade.php
+++
@@ -1,6 +0,0 @@
-@props([
-    'href' => null,
-    'request' => null
-])
-
-<a href="{{ $href }}" class="{{ $request === '1' ? 'text-gray-50' : 'text-gray-50/75' }} hover:text-xl transition-all duration-200 active:text-lg">{{ $slot }}</a>
